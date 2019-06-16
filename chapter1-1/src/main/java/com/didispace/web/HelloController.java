cc2bbaa4c06d348e125b04b2b83c57c65d6b0268
Administrator <admin@example.com>
2019-06-16 15:00:20 +0800

    one

--- chapter1-1/src/main/java/com/didispace/web/HelloController.java
+++ chapter1-1/src/main/java/com/didispace/web/HelloController.java
@@ -1,4 +1,4 @@
-package com.didispace.chapter11;
+package com.didispace.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
